9dda13661a27d0880650de53a7b264f3d5b1aa88
mugishap <precieuxmugisha@gmail.com>
2023-09-01 00:04:00 +0200

    [init]: Initialize project

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const http = require('http')
+const paypack = require("paypack-js");
+const bodyParser = require('body-parser')
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const { CLIENT_ID, CLIENT_SECRET,ENVIRONMENT } = process.env;
+app.use(express.json());
+app.use(bodyParser.json({}))
+
+paypack.config({
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET
+});
+
+app.post('/pay', async (req, res) => {
+    const { amount, phone } = req.body;
+    if (!amount || !phone) return res.status(400).json({ success: false, message: "Please provide amount and phone number" })
+    paypack.cashin({
+        number: phone,
+        amount: amount,
+        environment: ENVIRONMENT,
+    })
+        .then((data) => {
+            console.log(data.data);
+            return res.status(400).json({ success: true, message: "Payment initiated successfully" })
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(400).json({ success: false, message: err.message })
+        });
+})
+
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
