9382345b650f9a0b9979089b29fb7b885cf510ff
mugishap <precieuxmugisha@gmail.com>
2023-09-06 19:11:21 +0200

    [update]: port

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ app.post('/pay', async (req, res) => {
 })
 
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+server.listen(6060, () => {
+    console.log('Server is running on port 6060');
 });
